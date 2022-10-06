6f49ca9761578b8e684b32e7264a330d5a5adbfb
DanilByaliy <byaliy03@gmail.com>
2022-10-07 01:09:48 +0300

    Add .yaml files and fix

--- services/tracks/src/server.js
+++ services/tracks/src/server.js
@@ -2,8 +2,7 @@ require('dotenv').config();
 const app = require('./app');
 
 const EXPRESS_PORT = process.env.EXPRESS_PORT || 8080;
-const HOST = process.env.HOST || 'localhost';
 
-app.listen(EXPRESS_PORT, HOST, () => {
-  console.log(`Server listening on http://${HOST}:${EXPRESS_PORT}`);
+app.listen(EXPRESS_PORT, () => {
+  console.log(`Server listening on http://localhost:${EXPRESS_PORT}`);
 });
