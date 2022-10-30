0e6a3cb177466ae5896b98468e21b0d4eb5f9aba
Bogdan-Zinovij <76776361+Bogdan-Zinovij@users.noreply.github.com>
2022-10-30 23:59:04 +0200

    fix 'deployment.yaml' file

--- services/users/migrations/20221030101907_spending_init.up.sql
+++ services/users/migrations/20221030101907_spending_init.up.sql
@@ -1,15 +1,10 @@
-CREATE TABLE IF NOT EXISTS public.users
+CREATE TABLE IF NOT EXISTS users
 (
-    id character varying(64) COLLATE pg_catalog."default" NOT NULL,
-    name character varying(64) COLLATE pg_catalog."default" NOT NULL,
-    password character varying(64) COLLATE pg_catalog."default" NOT NULL,
-    email character varying(64) COLLATE pg_catalog."default" NOT NULL,
+    id character varying(64) NOT NULL,
+    name character varying(64) NOT NULL,
+    password character varying(64) NOT NULL,
+    email character varying(64) NOT NULL,
     "createdAt" timestamp with time zone NOT NULL,
     "updatedAt" timestamp with time zone NOT NULL,
     CONSTRAINT users_pkey PRIMARY KEY (id)
 )
-
-TABLESPACE pg_default;
-
-ALTER TABLE IF EXISTS public.users
-    OWNER to postgres;
