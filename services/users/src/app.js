8bae527cde18cb66d1747dc885e7393f0242bac9
Bogdan-Zinovij <76776361+Bogdan-Zinovij@users.noreply.github.com>
2022-10-30 12:17:05 +0200

    Added db config and 'Users' model

--- services/users/src/app.js
+++ services/users/src/app.js
@@ -7,6 +7,11 @@ const { PREFIX } = require('./config');
 const app = express();
 
 app.use(bodyParser.json());
-app.get(PREFIX + '/users', userController.getAll);
+app
+  .get(PREFIX + '/users', userController.getUsers)
+  .get(PREFIX + '/users/:id', userController.getUserById)
+  .post(PREFIX + '/users', userController.createUser)
+  .patch(PREFIX + '/users/:id', userController.updateUser)
+  .delete(PREFIX + '/users/:id', userController.deleteUser);
 
 module.exports = app;
