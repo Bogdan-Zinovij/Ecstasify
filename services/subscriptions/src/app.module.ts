731502a3bbfdd7ee39c8b02f987bdf4b7d4c05f9
KhrapkoVasyl <khrapko2002@gmail.com>
2022-10-30 11:10:09 +0200

    feat(subscriptions): implement database module by adding typeorm db and configure it

--- services/subscriptions/src/app.module.ts
+++ services/subscriptions/src/app.module.ts
@@ -1,7 +1,8 @@
 import { Module } from '@nestjs/common';
+import { DatabaseModule } from './database/database.module';
 import { SubscriptionsModule } from './modules/subscriptions/subscriptions.module';
 
 @Module({
-  imports: [SubscriptionsModule],
+  imports: [DatabaseModule, SubscriptionsModule],
 })
 export class AppModule {}
