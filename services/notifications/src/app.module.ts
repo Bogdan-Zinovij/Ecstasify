f4e40c08acaf8e0dd89acd0d346b446cc077922d
KhrapkoVasyl <khrapko2002@gmail.com>
2023-01-07 20:11:06 +0200

    chore(notifications): remove unnecessary auto-generated files

--- services/notifications/src/app.module.ts
+++ services/notifications/src/app.module.ts
@@ -1,10 +1,9 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { MailModule } from './mail/mail.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [MailModule],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
