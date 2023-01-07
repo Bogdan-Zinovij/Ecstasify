8ac8fded393ea764df300612d8915b1d5d1e02b6
KhrapkoVasyl <khrapko2002@gmail.com>
2023-01-07 19:58:17 +0200

    feat(notifications): change service base url and host

--- services/notifications/src/main.ts
+++ services/notifications/src/main.ts
@@ -2,7 +2,16 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
+  const PORT = 8080;
+  const HOST = '127.0.0.1';
+  const GLOBAL_PREFIX = '/api/v1';
+
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+
+  app.setGlobalPrefix(GLOBAL_PREFIX);
+
+  await app.listen(PORT, () => {
+    console.log(`Server listens on http://${HOST}:${PORT}`);
+  });
 }
 bootstrap();
