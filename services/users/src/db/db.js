e0beea7eb90c3ad9e3d80ea57e78b6cd09250d92
Bogdan-Zinovij <76776361+Bogdan-Zinovij@users.noreply.github.com>
2023-01-08 19:25:33 +0200

    change db credentials

--- services/users/src/db/db.js
+++ services/users/src/db/db.js
@@ -1,13 +1,6 @@
 import { Sequelize } from 'sequelize';
-import * as dotenv from 'dotenv';
-dotenv.config();
 
-export default new Sequelize(
-  process.env.DB_NAME,
-  process.env.DB_USER,
-  process.env.DB_PASSWORD,
-  {
-    host: process.env.DB_HOST,
-    dialect: process.env.DB_DIALECT,
-  },
-);
+export default new Sequelize('demo', 'demo', 'demo', {
+  host: 'postgres-users',
+  dialect: 'postgres',
+});
