defd6c622e45f87c21e8cf163a44bc338434a387
KhrapkoVasyl <khrapko2002@gmail.com>
2023-01-10 23:26:09 +0200

    refactor(subscription-plan): rewrite service extending base class

--- services/subscriptions/src/modules/subscriptions/services/subscription-plans.service.ts
+++ services/subscriptions/src/modules/subscriptions/services/subscription-plans.service.ts
@@ -1,80 +1,23 @@
-import {
-  BadRequestException,
-  Injectable,
-  NotFoundException,
-} from '@nestjs/common';
+import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { ErrorMessagesEnum } from 'src/common';
-import { FindOptionsWhere, Repository } from 'typeorm';
+import { ServiceErrorMessagesInterface } from 'src/common/interfaces';
+import { BaseService } from 'src/common/services';
+import { Repository } from 'typeorm';
 import { SubscriptionPlanEntity } from '../entities';
 
+const serviceErrorMessages: ServiceErrorMessagesInterface = {
+  entitiesNotFound: ErrorMessagesEnum.SUBSCRIPTION_PLANS_NOT_FOUND,
+  entityNotFound: ErrorMessagesEnum.SUBSCRIPTION_PLAN_NOT_FOUND,
+  inputDataError: ErrorMessagesEnum.INPUT_DATA_ERROR,
+};
+
 @Injectable()
-export class SubscriptionPlansService {
+export class SubscriptionPlansService extends BaseService<SubscriptionPlanEntity> {
   constructor(
     @InjectRepository(SubscriptionPlanEntity)
-    private readonly subscriptionPlanEntityRepository: Repository<SubscriptionPlanEntity>,
-  ) {}
-
-  findAll(): Promise<SubscriptionPlanEntity[]> {
-    return this.subscriptionPlanEntityRepository.find().catch(() => {
-      throw new NotFoundException(
-        ErrorMessagesEnum.SUBSCRIPTION_PLANS_NOT_FOUND,
-      );
-    });
-  }
-
-  findOne(
-    conditions: FindOptionsWhere<SubscriptionPlanEntity>,
-  ): Promise<SubscriptionPlanEntity> {
-    return this.subscriptionPlanEntityRepository
-      .findOne({
-        where: conditions,
-      })
-      .catch(() => {
-        throw new NotFoundException(
-          ErrorMessagesEnum.SUBSCRIPTION_PLAN_NOT_FOUND,
-        );
-      });
-  }
-
-  async createOne(
-    entity: Partial<SubscriptionPlanEntity>,
-  ): Promise<SubscriptionPlanEntity> {
-    const createdEntity = this.subscriptionPlanEntityRepository.create(entity);
-    const { id } = await this.subscriptionPlanEntityRepository
-      .save(createdEntity)
-      .catch(() => {
-        throw new BadRequestException(ErrorMessagesEnum.INPUT_DATA_ERROR);
-      });
-    return this.findOne({ id });
-  }
-
-  async updateOne(
-    conditions: FindOptionsWhere<SubscriptionPlanEntity>,
-    entity: Partial<SubscriptionPlanEntity>,
-  ): Promise<SubscriptionPlanEntity> {
-    const entityToUpdate = await this.findOne(conditions);
-    const updatedEntity = this.subscriptionPlanEntityRepository.merge(
-      entityToUpdate,
-      entity,
-    );
-    const { id } = await this.subscriptionPlanEntityRepository
-      .save(updatedEntity)
-      .catch(() => {
-        throw new BadRequestException(ErrorMessagesEnum.INPUT_DATA_ERROR);
-      });
-    return this.findOne({ id });
-  }
-
-  async deleteOne(
-    conditions: FindOptionsWhere<SubscriptionPlanEntity>,
-  ): Promise<SubscriptionPlanEntity> {
-    const entity = await this.findOne(conditions);
-
-    return this.subscriptionPlanEntityRepository.remove(entity).catch(() => {
-      throw new NotFoundException(
-        ErrorMessagesEnum.SUBSCRIPTION_PLAN_NOT_FOUND,
-      );
-    });
+    subscriptionPlanEntityRepository: Repository<SubscriptionPlanEntity>,
+  ) {
+    super(subscriptionPlanEntityRepository, serviceErrorMessages);
   }
 }
