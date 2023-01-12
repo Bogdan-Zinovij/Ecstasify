9d951de174051c0cadf7d0c4d6a03545bcce6ee2
KhrapkoVasyl <khrapko2002@gmail.com>
2023-01-12 13:01:04 +0200

    fix: remove incorrect handling of 'new track created' and 'get all users' topics

--- services/notifications/src/modules/kafka/kafka.service.ts
+++ services/notifications/src/modules/kafka/kafka.service.ts
@@ -1,27 +1,11 @@
-import { Inject, Injectable } from '@nestjs/common';
-import { ClientKafka } from '@nestjs/microservices';
+import { Injectable } from '@nestjs/common';
 import { MailContextInterface } from '../mail/interfaces/mail-context.interface';
 import { MailService } from '../mail/mail.service';
-import {
-  GetUserDto,
-  HandleNewTrackCreatedDto,
-  HandleUserRegisteredDto,
-} from './dto';
-import { TopicEnum } from './enums';
-import { KafkaMessageInterface } from './interfaces';
-import { CLIENT_KAFKA_SERVICE_NAME } from './kafka.constants';
+import { HandleUserRegisteredDto } from './dto';
 
 @Injectable()
 export class KafkaService {
-  constructor(
-    @Inject(CLIENT_KAFKA_SERVICE_NAME)
-    private readonly clientKafka: ClientKafka,
-    private readonly mailService: MailService,
-  ) {}
-
-  subscribeToResponseOf(topic: TopicEnum) {
-    this.clientKafka.subscribeToResponseOf(topic);
-  }
+  constructor(private readonly mailService: MailService) {}
 
   handleNewUserRegistered(value: string) {
     const user: HandleUserRegisteredDto = JSON.parse(value);
@@ -33,25 +17,5 @@ export class KafkaService {
     };
 
     this.mailService.sendTemplatedMail('user-registered', [email], context);
-  }
-
-  handleNewTrackCreated(value: string) {
-    console.log('\n\n Track recieved: ', value, '\n\n');
-    const track: HandleNewTrackCreatedDto = JSON.parse(value);
-    this.clientKafka
-      .send(TopicEnum.GET_ALL_USERS, {})
-      .subscribe((message: KafkaMessageInterface) => {
-        const users: GetUserDto[] = JSON.parse(message?.value);
-
-        console.log('\n\n Users recieved: ', users, '\n\n');
-
-        const emails = users.map((user) => user.email);
-
-        const context: MailContextInterface = {
-          trackName: track?.name,
-        };
-
-        this.mailService.sendTemplatedMail('track-created', emails, context);
-      });
   }
 }
