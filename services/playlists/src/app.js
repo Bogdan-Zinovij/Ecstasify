c492f6daba0701f4322f156bc5128e3ebc287e86
DanilByaliy <byaliy03@gmail.com>
2023-01-13 00:15:49 +0200

    Add routes for playlists and playlistTracks

--- services/playlists/src/app.js
+++ services/playlists/src/app.js
@@ -3,18 +3,15 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
-const playlistController = require('./controllers/playlistController');
+const playlistTracksRouter = require('./routers/playlistTracksRouter');
+const playlistsRouter = require('./routers/playlistsRouter');
 const { PREFIX } = require('./config');
 const app = express();
 
 app.use(bodyParser.json());
 app.use(cors());
 
-app
-  .get(PREFIX + '/playlists', playlistController.getPlaylists)
-  .get(PREFIX + '/playlists/:id', playlistController.getPlaylistById)
-  .post(PREFIX + '/playlists', playlistController.createPlaylist)
-  .patch(PREFIX + '/playlists/:id', playlistController.updatePlaylist)
-  .delete(PREFIX + '/playlists/:id', playlistController.deletePlaylist);
+app.use(PREFIX + '/playlists/playlistTracks', playlistTracksRouter);
+app.use(PREFIX + '/playlists', playlistsRouter);
 
 module.exports = app;
