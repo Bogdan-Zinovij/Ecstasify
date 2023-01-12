11febded60a71d3d79fdb6c98c389591331459dc
KhrapkoVasyl <khrapko2002@gmail.com>
2023-01-12 15:30:26 +0200

    refactor(subscriptions): move settings values to environment variables

--- services/subscriptions/src/app.module.ts
+++ services/subscriptions/src/app.module.ts
@@ -1,8 +1,9 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { DatabaseModule } from './database/database.module';
 import { SubscriptionsModule } from './modules/subscriptions/subscriptions.module';
 
 @Module({
-  imports: [DatabaseModule, SubscriptionsModule],
+  imports: [ConfigModule.forRoot(), DatabaseModule, SubscriptionsModule],
 })
 export class AppModule {}
--- services/subscriptions/src/database/database.module.ts
+++ services/subscriptions/src/database/database.module.ts
@@ -1,22 +1,27 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config/dist';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { dirname } from 'path';
 
 @Module({
   imports: [
-    TypeOrmModule.forRoot({
-      type: 'postgres',
-      host: 'postgres-subscriptions',
-      port: 5432,
-      logging: true,
-      username: 'postgres',
-      password: 'postgres',
-      database: 'demo',
-      entities: [dirname(__dirname) + '/modules/**/*.entity.{ts,js}'],
-      migrations: [__dirname + '/migrations/*.{ts,js}'],
-      migrationsRun: false,
-      dropSchema: false,
-      synchronize: false,
+    TypeOrmModule.forRootAsync({
+      useFactory: (configService: ConfigService) => ({
+        type: configService.get<'postgres'>('TYPEORM_TYPE'),
+        host: configService.get('TYPEORM_HOST'),
+        port: configService.get('TYPEORM_PORT'),
+        logging: configService.get('TYPEORM_LOGGING'),
+        username: configService.get('TYPEORM_USERNAME'),
+        password: configService.get('TYPEORM_PASSWORD'),
+        database: configService.get<string>('TYPEORM_DATABASE'),
+        entities: [dirname(__dirname) + '/modules/**/*.entity.{ts,js}'],
+        migrations: [__dirname + '/migrations/*.{ts,js}'],
+        migrationsRun: configService.get('TYPEORM_MIGRATIONS_RUN'),
+        dropSchema: configService.get('TYPEORM_DROP_SCHEMA'),
+        synchronize: configService.get('TYPEORM_SYNCHRONIZE'),
+      }),
+      imports: [ConfigModule],
+      inject: [ConfigService],
     }),
   ],
 })
