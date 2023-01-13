8651660cd695eb89f70bce6ac66705381ef0acd3
DanilByaliy <byaliy03@gmail.com>
2023-01-13 10:43:42 +0200

    Add changes according to the review

--- services/playlists/src/server.js
+++ services/playlists/src/server.js
@@ -2,7 +2,8 @@ require('dotenv').config();
 const app = require('./app');
 const { associate } = require('./db/associate');
 const { db } = require('./db/db');
-const EXPRESS_PORT = process.env.EXPRESS_PORT || 8080;
+const EXPRESS_PORT = process.env.EXPRESS_PORT;
+const EXPRESS_HOST = process.env.EXPRESS_HOST;
 
 (async () => {
   try {
@@ -12,6 +13,6 @@ const EXPRESS_PORT = process.env.EXPRESS_PORT || 8080;
     console.log(err);
   }
   app.listen(EXPRESS_PORT, () => {
-    console.log(`Server listening on http://localhost:${EXPRESS_PORT}`);
+    console.log(`Server listening on http://${EXPRESS_HOST}:${EXPRESS_PORT}`);
   });
 })();
