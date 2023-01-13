e72e5db3e70bbeb307e675cd10dddf8b5d38746b
KhrapkoVasyl <khrapko2002@gmail.com>
2023-01-13 09:44:00 +0200

    chore(files): add server port, host and global prefix in env variables

--- services/files/src/main.ts
+++ services/files/src/main.ts
@@ -1,8 +1,19 @@
+import { ConfigService } from '@nestjs/config/dist';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const configService = app.get<ConfigService>(ConfigService);
+
+  const HOST = configService.get('NEST_HOST');
+  const PORT = configService.get('NEST_PORT');
+  const GLOBAL_PREFIX = configService.get('GLOBAL_PREFIX');
+
+  app.setGlobalPrefix(GLOBAL_PREFIX);
+
+  await app.listen(PORT, HOST, () => {
+    console.log(`Server listens on http://${HOST}:${PORT}`);
+  });
 }
 bootstrap();
