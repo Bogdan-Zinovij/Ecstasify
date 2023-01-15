abd40b34dbc364bce53074bcd8319f57b426a7f1
Bogdan-Zinovij <76776361+Bogdan-Zinovij@users.noreply.github.com>
2023-01-15 18:00:42 +0200

    move author validation values into 'config.js'

--- services/authors/src/config.js
+++ services/authors/src/config.js
@@ -1,5 +1,11 @@
 export const PREFIX = '/api/v1';
+
 export const errorMessages = {
   AUTHOR_ALREADY_EXISTS: 'Author with the same name has already exists',
   AUTHOR_NOT_EXISTS: 'Author with the specified ID does not exists',
+};
+
+export const authorValidationConfig = {
+  MIN_NAME_LENGTH: 2,
+  MAX_NAME_LENGTH: 20,
 };
