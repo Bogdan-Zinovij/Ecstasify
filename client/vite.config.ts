ec2cc0702deae2998246700f4dc8f3e38ab55957
artemmatiushenko1 <artom.matyushenko@gmail.com>
2023-01-15 06:30:11 +0200

    Make use of env variables in vite config

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,23 +1,27 @@
-import { defineConfig } from 'vite';
+import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
 import path from 'path';
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, 'src'),
-    },
-  },
-  server: {
-    proxy: {
-      '/api/v1': {
-        target: 'http://localhost',
-        changeOrigin: true,
-        secure: false,
-        ws: true,
+export default ({ mode }) => {
+  const env = { ...process.env, ...loadEnv(mode, process.cwd()) };
+
+  return defineConfig({
+    plugins: [react()],
+    resolve: {
+      alias: {
+        '@': path.resolve(__dirname, 'src'),
       },
     },
-  },
-});
+    server: {
+      proxy: {
+        [process.env.VITE_API_URL]: {
+          target: env.VITE_API_HOST,
+          changeOrigin: true,
+          secure: false,
+          ws: true,
+        },
+      },
+    },
+  });
+};
