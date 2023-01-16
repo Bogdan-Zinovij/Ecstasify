a8891ec31ee5db81b8838bd5206309bd642b3e09
Bogdan-Zinovij <76776361+Bogdan-Zinovij@users.noreply.github.com>
2023-01-16 20:43:10 +0200

    add middleware that handle validation errors in 'users' service

--- services/users/src/routers/auth.router.js
+++ services/users/src/routers/auth.router.js
@@ -4,12 +4,13 @@ import {
   signInValidScheme,
   signUpValidScheme,
 } from '../middlewares/validation/auth.validation.js';
+import validateRequest from '../middlewares/validation/validate-request.js';
 
 const authRouter = new Router();
 
 authRouter
-  .post('/sign-up', signUpValidScheme, authController.signUp)
-  .post('/sign-in', signInValidScheme, authController.signIn)
+  .post('/sign-up', signUpValidScheme, validateRequest, authController.signUp)
+  .post('/sign-in', signInValidScheme, validateRequest, authController.signIn)
   .post('/sign-out', authController.signOut)
   .post('/refresh', authController.refresh)
   .post('/verify/:token', authController.verifyToken);
